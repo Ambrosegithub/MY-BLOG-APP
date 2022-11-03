15c8124e9f4f0a3193b920d659c8dd8d7c24b181
Isigba Ambrose <ambrisexyz@gmail.com>
2022-11-03 01:57:24 +0100

    added form#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    # @post = Post.all
+    @post = Post.all
   end
 
   def show
-    @post = Post.find(params[:id].to_i)
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = current_user
   end
 end
