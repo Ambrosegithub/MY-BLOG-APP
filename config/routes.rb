11a8bbfe03191fc48805c8addb99b2dece54b9a8
Isigba Ambrose <ambrisexyz@gmail.com>
2022-11-09 23:28:53 +0100

    made change in devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "users#show"
+  root to: "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :likes, only: [:create]
